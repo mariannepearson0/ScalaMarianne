9f563bc43c4e6ae1d03c379b359b3d681c9b5041
mariannepearson0 <mariannepearson@gmail.com>
2017-10-31 16:16:57 +0000

    Commiting and pushing after testing of brokenKeyboard

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,2 +1,7 @@
 object Main extends App {
+
+  val broke = new BrokenKeyboard
+  val fileName = "C:\\Users\\marianne\\IdeaProjects\\Scala_Marianne\\src\\main\\scala\\brokenKeyboardInput2.txt"
+  val fileNameWords = "C:\\Users\\marianne\\IdeaProjects\\Scala_Marianne\\src\\main\\scala\\WordsList.txt"
+  println(broke.longestWord(fileName, fileNameWords))
 }
