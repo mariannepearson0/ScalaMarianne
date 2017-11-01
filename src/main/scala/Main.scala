48abaffc3023ba597612576603fa7dd47712fc55
mariannepearson0 <mariannepearson@gmail.com>
2017-11-01 13:01:41 +0000

    Commiting and pushing after big number to string converter

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,6 +5,9 @@ object Main extends App {
 //  val fileNameWords = "C:\\Users\\marianne\\IdeaProjects\\Scala_Marianne\\src\\main\\scala\\WordsList.txt"
 //  println(broke.longestWord(fileName, fileNameWords))
 
-    val lumber = new Lumberjack
-  lumber.placeLogs()
+   // val lumber = new Lumberjack
+  //lumber.placeLogs()
+  val numby = new BigNumbers
+  println(numby.makeSomeWords(numby.bigNumberConverter("0676234560")))
+
 }
---
+++ src/main/scala/MorseCode.scala
@@ -0,0 +1,36 @@
+class MorseCode extends Output {
+
+  //MORSE CODE
+  val morseMap = Map("a"->"._", "b"->"_...", "c"->"_._.",
+    "d"->"_..", "e"->".", "f"->".._.", "g"->"__.", "h"->"....",
+    "i"->"..", "j"->".___", "k"->"_._", "l"->"._..", "m"->"__",
+    "n"->"_.", "o"->"___", "p"->".__.", "q"->"__._", "r"->"._.",
+    "s"->"...", "t"->"_", "u"->".._", "v"->"..._", "w"->".__",
+    "x"->"_.._", "y"->"_.__", "z"->"__..", " "-> " ")
+
+  def keyForValue(value: String): String = {
+    val revMap = morseMap map {_.swap}
+    val key = revMap(value)
+    key
+  }
+
+  def morseToString(morse:String):String = {
+    var message = ""
+    for (morse <- morse.split(" / ")) {
+      message += keyForValue(morse)
+    }
+    message
+  }
+
+  def stringToMorse(message:String): String = {
+    var morse = ""
+    for(letter <- message.toLowerCase) {
+      morse += morseMap.getOrElse(letter.toString, "") + " / "
+    }
+    morse
+  }
+
+  //morseToString(".__ / ___ / _. / _.. / . / ._. / .._. / .._ / ._..")
+  //stringToMorse("princess")
+
+}
