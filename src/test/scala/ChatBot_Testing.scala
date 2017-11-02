b47f712f8dc91de9d7182383215fc34a8b4f8e68
mariannepearson0 <mariannepearson@gmail.com>
2017-11-02 14:32:23 +0000

    Commiting and pushing after Tadas comments

--- src/test/scala/ChatBot_Testing.scala
+++ src/test/scala/ChatBot_Testing.scala
@@ -1,20 +1,7 @@
-import java.io.ByteArrayInputStream
-
-import scala.io.StdIn
 
 class ChatBot_Testing extends Base {
 
   val chatbot = new ChatBot
-
-//  "chatbot" should "give different replies based on user input" in {
-//    val in = new ByteArrayInputStream("Ramona".getBytes)
-//    System.setIn(in)
-//    //chatbot.playChatBot
-//    //StdIn.readLine()
-//    val loops = new ChatBot with MockOutput
-//    loops.playChatBot
-//    println(loops.messages)
-//  }
 
   trait MockOutput extends Output {
     var messages: Seq[String] = Seq()
--- src/test/scala/Lumberjack_Testing.scala
+++ src/test/scala/Lumberjack_Testing.scala
@@ -4,7 +4,6 @@ class Lumberjack_Testing extends Base{
 
   "findLowest method" should "return the lowest number from a dimensional integer array" in {
     lumberjack.findLowest() should equal (1)
-    //lumberjack.findLowest(Array(Array(3, 3, 3), Array(2, 2, 3), Array(2, 4, 5))) should equal (2)
   }
 
   //Test is failing to start
@@ -12,7 +11,6 @@ class Lumberjack_Testing extends Base{
     val loops = new Lumberjack with MockOutput
     loops.placeLogs()
     println(loops.messages)
-    //loops.messages should equal ("322", "223", "242")
   }
 
   trait MockOutput extends Output {
