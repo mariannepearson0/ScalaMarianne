daf959d3b47e214a9c9d4885bc08d456e6ad4eb9
mariannepearson0 <mariannepearson@gmail.com>
2017-11-02 14:27:23 +0000

    Commiting and pushing after some Db implementation

--- src/main/scala/Databases/MainDB.scala
+++ src/main/scala/Databases/MainDB.scala
@@ -1,5 +1,103 @@
 package Databases
 
-class MainDB {
+import slick.jdbc.MySQLProfile.api._
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.util.{Failure, Success}
 
+object MainDB extends App {
+
+  val db = Database.forConfig("mysqlDB")
+
+  val peopleTable = TableQuery[People]
+
+  val dropPeopleCmd = DBIO.seq(peopleTable.schema.drop)
+
+  val initPeopleCmd = DBIO.seq(peopleTable.schema.create)
+
+  def dropDB = {
+    //do a drop followed by initialisePeople
+    val dropFuture = Future{db.run(dropPeopleCmd)}
+    //Attempt to drop the table, Await does not block here
+    Await.result(dropFuture, Duration.Inf).andThen{
+      case Success(_) =>  initialisePeople
+      case Failure(error) => println("Dropping the table failed due to: " + error.getMessage)
+        initialisePeople}
+  }
+
+  def initialisePeople = {
+    //initialise people
+    val setupFuture =  Future {db.run(initPeopleCmd)}
+    //once our DB has finished initializing we are ready to roll, Await does not block
+    Await.result(setupFuture, Duration.Inf).andThen{
+      case Success(_) => runQuery
+      case Failure(error) => println("Initialising the table failed due to: " + error.getMessage)}
+  }
+
+  def runQuery = {
+    val insertPeople = Future {
+      val query = peopleTable ++= Seq(
+        (10, "Jack", "Wood", 36),
+        (20, "Tim", "Brown", 24))
+      // insert into `PEOPLE` (`PER_FNAME`,`PER_LNAME`,`PER_AGE`)  values (?,?,?)
+      println(query.statements.head) // would print out the query one line up
+      db.run(query)
+    }
+      Await.result(insertPeople, Duration.Inf).andThen {
+        case Success(_) => deletePeople
+        case Failure(error) => println("Welp! Something went wrong! " + error.getMessage)}
+  }
+
+  def listPeople = {
+    val queryFuture = Future {
+      // simple query that selects everything from People and prints them out
+      db.run(peopleTable.result).map(_.foreach {
+        case (id, fName, lName, age) => println(s" $id $fName $lName $age")
+      })
+    }
+    Await.result(queryFuture, Duration.Inf).andThen {
+      case Success(_) =>  db.close()  //cleanup DB connection
+      case Failure(error) => println("Listing people failed due to: " + error.getMessage)}
+  }
+
+  def deletePeople = {
+    val d = peopleTable.filter(_.fName === "Jack")
+    val action = d.delete
+    val deleteP = Future {
+      db.run(action)
+    }
+    Await.result(deleteP, Duration.Inf).andThen {
+      case Success(_) => updatePeople
+      case Failure(error) => println("Uh Oh! Something went wrong! " + error.getMessage)}
+  }
+
+  def updatePeople = {
+    val age = for { p <- peopleTable if p.fName === "Tim"} yield p.age
+    val updateAction = age.update(30)
+    val updatep = Future {
+      db.run(updateAction)
+    }
+    Await.result(updatep, Duration.Inf).andThen {
+      case Success(_) => listPeople
+      case Failure(error) => println("Wuh-oh! Something went wrong! " + error.getMessage)}
+  }
+
+  def searchPeople = {
+    val search = Future {
+      db.run(peopleTable.result).map(_.foreach {
+        case (id, fName, lName, age) => println(s" $id $fName $lName $age")
+      //peopleTable.filter(_.lName === "Brown")
+    })
+    }
+  }
+
+  dropDB
+//  initialisePeople
+//  Thread.sleep(100)
+//  deletePeople
+//  Thread.sleep(100)
+//  updatePeople
+//  Thread.sleep(100)
+//  listPeople
 }
---
+++ src/main/scala/Databases/People.scala
@@ -0,0 +1,14 @@
+package Databases
+
+import slick.driver.MySQLDriver.api._
+
+class People(tag: Tag) extends Table[(Int, String, String, Int)](tag, "PEOPLE"){
+  def id = column[Int]("PER_ID", O.PrimaryKey, O.AutoInc)
+  def fName = column[String]("PER_FNAME")
+  def lName = column[String]("PER_LNAME")
+  def age = column[Int]("PER_AGE")
+  def * = (id, fName, lName, age)
+}
+
+
+
